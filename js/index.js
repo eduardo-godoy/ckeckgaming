281ddfcb3c70b5d96dd407d4f06db25bc522f8c1
Eduardo Godoy <eduardogodoy.dw@gmail.com>
2023-11-20 15:08:32 -0300

    PreEntrega3+Godoy

--- js/index.js
+++ js/index.js
@@ -8,8 +8,12 @@ const totalSection = document.querySelector('section.total-section')
 const btnCart = document.querySelector('a.a-cart')
 const logo = document.querySelector('a.logo-a')
 
+// tomamos el carrito de localstorage y lo convertimos en array
+
 let carrito = JSON.parse(localStorage.getItem('carrito')) || []
 
+
+// funciones del index, renderizado de todos los productos, mas la funcion del agregado al carrito 
 
 function crearCardError() {
     return `<div class="div-card-error">
@@ -19,14 +23,17 @@ function crearCardError() {
            </div>`
 }
 
-function crearCardHTML(producto) {
+function crearCardHTML({id, imagen, titulo, precio}) {
     return `<article class='article-card'>
-                <img src='${producto.imagen}' class='imagen'/>
-                <h2 class='h2-titulo'>${producto.titulo}</h2>
-                <h3 class='h2-titulo'>Precio: $${producto.precio}</h3>
-                <button id="${producto.id}" class='btn'>Agregar</button>
+                <img src='${imagen}' class='imagen'/>
+                <h2 class='h2-titulo'>${titulo}</h2>
+                <h3 class='h2-titulo'>Precio: $${precio}</h3>
+                <button id="${id}" class='btn'>Agregar</button>
             </article>`
 }
+
+// funcion que agrega al carrito los productos, tomandolos por su id y pusheandolos en el array de carrito, por ultimo se convierte en 
+// un json y se guarda en local storage
 
 function activarClickEnBotones() {
     const botonesAgregar = document.querySelectorAll("button.btn")
@@ -35,7 +42,7 @@ function activarClickEnBotones() {
             const id = parseInt(e.target.id)
             const productoSeleccionado = productos.find((producto) => producto.id === id)
             carrito.push(productoSeleccionado)
-            numeroCarrito.innerHTML = carrito.length
+            numeroCarrito.innerHTML = carrito.length || null
             localStorage.setItem("carrito", JSON.stringify(carrito))
         })
     })
@@ -54,6 +61,8 @@ function cargarProductos() {
 }
 
 cargarProductos()
+
+// eventos del input y el navbar, filtra por lo q escibre el usuario o haciendo click en computadora o notebook te filtra por la categoria.
 
 inputBuscar.addEventListener('search', ()=> {
     let textoAbuscar = inputBuscar.value.trim().toLowerCase()
@@ -89,15 +98,17 @@ botonComputadora.addEventListener('click', () => {
     activarClickEnBotones()
 })
 
+// funciones y eventos del carrito, haciendo click en el carrito esquina superior derecha
+
 function crearCardHTMLError() {
     return `<h2>No hay productos en el carrito</h2>`
 } 
 
-function crearCardHTMLCart(producto) {
+function crearCardHTMLCart({imagen, titulo, precio}) {
     return `<article class='article-card-cart'>
-                <img src='${producto.imagen}' class='imagen-cart'/>
-                <h2 class='h2-titulo-cart'>${producto.titulo}</h2>
-                <h3 class='h3-titulo-cart'>Precio: $${producto.precio}</h3>
+                <img src='${imagen}' class='imagen-cart'/>
+                <h2 class='h2-titulo-cart'>${titulo}</h2>
+                <h3 class='h3-titulo-cart'>Precio: $${precio}</h3>
             </article>`
 }
 
@@ -112,6 +123,7 @@ function cargarProductosEnCarrito() {
         tituloIndex.innerHTML = 'Carrito'
         contenedorIndex.innerHTML = ''
         carrito.forEach((producto) => contenedorIndex.innerHTML += crearCardHTMLCart(producto))
+        numeroCarrito.innerHTML = carrito.length || null
         mostrarTotal()
         totalSection
     } else {
@@ -119,6 +131,8 @@ function cargarProductosEnCarrito() {
         contenedorIndex.innerHTML = crearCardHTMLError()
     }
 }
+
+// evento del dom para acceder al carrito
 
 btnCart.addEventListener('click', cargarProductosEnCarrito)
 
